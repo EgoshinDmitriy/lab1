a5a435afa3881a9162c524678cedbf26a6241471
dima egoshin <dima.dima.00.00@bk.ru>
2017-12-27 23:27:32 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace PassAuth
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+
+        {
+            InitializeComponent();
+            FileInfo fileInf = new FileInfo("pass.txt");
+            if (fileInf.Exists)
+                groupBox1.Visible = true;
+        }
+
+        private int i = 3;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string pas = textBox1.Text;
+            string pass;
+            FileInfo fileInf = new FileInfo("pass.txt");
+            if (fileInf.Exists)
+            {
+                FileStream file = new FileStream("pass.txt", FileMode.Open);
+                StreamReader reader = new StreamReader(file);
+                pass = reader.ReadLine();
+                reader.Close();
+                key Key = new key();
+                //int[] kl = textBox2.Text.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToArray();
+                int[] kl = new int[textBox2.Text.Length];
+                char[] chArray = textBox2.Text.ToCharArray();
+                for (int i = 0; i < textBox2.Text.Length; ++i)
+                {
+                    kl[i] = char.Parse("" + textBox2.Text[i]);
+                }
+                Key.Val = kl;
+                pas = Encrypt(pas,kl);
+                if (pas == pass)
+                {
+                    Form ifrm = new Form2(Key);
+                    ifrm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    i--;
+                    MessageBox.Show("Wrong password or key! You have " + i + " chances!");
+                }
+            }
+            else
+            {
+                FileStream file = new FileStream("pass.txt", FileMode.Create);
+                StreamWriter writer = new StreamWriter(file);
+                Random rnd = new Random();
+               
+                int i = 1 + rnd.Next(100);
+                int[] kl= GenerateKeyWord(i);
+                string str = "";
+                for (i = 0; i < kl.Length; i++)
+                {
+                    str += kl[i].ToString() + " ";
+                }
+                key Key = new key();
+                Key.Val = kl;
+                pas = Encrypt(pas, kl);
+                writer.WriteLine(pas);
+                MessageBox.Show("Your key: " + str);
+                writer.Close();
+                Form ifrm = new Form2(Key);
+                ifrm.Show();
+                this.Hide();
+            }
+            if (i == 0)
+            {
+                this.Close();
+            }
+        }
+        public string Encrypt(string input, int[] key)
+        {
+            for (int i = 0; i < input.Length % 4; i++)
+                input += input[i];
+
+            string result = "";
+
+            for (int i = 0; i < input.Length; i += 4)
+            {
+                char[] transposition = new char[4];
+
+                for (int j = 0; j < 4; j++)
+                    transposition[key[j] - 1] = input[i + j];
+
+                for (int j = 0; j < 4; j++)
+                    result += transposition[j];
+            }
+
+            return result;
+        }
+
+        public int[] GenerateKeyWord(int startSeed)
+        {
+            int[] key = new int[4];
+            Random rand = new Random(startSeed);
+            for (int i = 0; i < 4; i++)
+                key[i] = rand.Next(1, 4);
+            return key;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class key
+    {
+        private int[] val;
+        public int[] Val
+        { 
+            get { return val; }
+          set { val = value; }
+        }
+    }
+ }
+
+
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace PassAuth
+{
+    public partial class Form2 : Form
+    {
+        public static key kl;
+        public Form2(key K)
+        {
+            InitializeComponent();
+            kl = K;
+        }
+
+        private int i = 3;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string oldpas = textBox1.Text;
+            string newpas = textBox2.Text;
+            string pass;
+            FileStream file = new FileStream("pass.txt", FileMode.Open);
+            StreamReader reader = new StreamReader(file);
+            pass = reader.ReadLine();
+            reader.Close();
+           /* int[] key = textBox3.Text.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToArray();*/
+            oldpas = Encrypt(oldpas, kl.Val);
+            if (pass == oldpas)
+            {
+                FileStream wfile = new FileStream("pass.txt", FileMode.Truncate);
+                StreamWriter writer = new StreamWriter(wfile);
+                Random rnd = new Random();
+                int i = 1 + rnd.Next(100);
+                kl.Val = GenerateKeyWord(i);
+                string str = "";
+                for (i = 0; i < kl.Val.Length; i++)
+                {
+                    str += kl.Val[i].ToString() + " ";
+                }
+                newpas = Encrypt(newpas, kl.Val);
+                writer.WriteLine(newpas);
+                writer.Close();
+                MessageBox.Show("Your new key: " + str);
+            }
+            else
+            {
+                i--;
+                MessageBox.Show("Wrong password or key! You have " + i + " chances!");
+            }
+        }
+
+        public static char[] characters = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+           'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','X','X','0','1','2','3','4','5','6','7','8','9','_'};
+
+        public int N = characters.Length;
+
+        public string Encrypt(string input, int[] key)
+        {
+            for (int i = 0; i < input.Length % 4; i++)
+                input += input[i];
+
+            string result = "";
+
+            for (int i = 0; i < input.Length; i += 4)
+            {
+                char[] transposition = new char[4];
+
+                for (int j = 0; j < 4; j++)
+                    transposition[key[j] - 1] = input[i + j];
+
+                for (int j = 0; j < 4; j++)
+                    result += transposition[j];
+            }
+
+            return result;
+        }
+
+        public int[] GenerateKeyWord(int startSeed)
+        {
+            int[] key = new int[4];
+            Random rand = new Random(startSeed);
+            for (int i = 0; i < 4; i++)
+                key[i] = rand.Next(1, 4);
+            return key;
+        }
+
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form ifrm = Application.OpenForms[0];
+            ifrm.Show();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+            private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+            {
+                Application.Exit();
+            }
+    }
+}
